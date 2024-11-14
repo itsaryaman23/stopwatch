8ee3e83a87aa2e40cb91e5fc104d33c4b5d93d93
Test <itsaryaman23@gmail.com>
2024-11-14 12:24:58 +0530

    Stopwatch

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  const [intervalTracker, setIntervalTracker] = useState(-1);
+  const [time, setTime] = useState(0);
+  const format = (t)=>{
+    const min = String(Math.floor(time/60)).padStart(2,0);
+    const sec = String(time%60).padStart(2,0);
+    return `${min}:${sec}`;
+
+  }
+
+  return (
+    <div class="App">
+      <h1>StopWatch</h1>
+      <p>Time: {format(time)}</p>
+      {
+        intervalTracker==-1?
+        <button onClick = {()=>{
+          let t = setInterval(()=>{
+            setTime(t=>t+1);
+          },100)
+          setIntervalTracker(t);
+        }}>Start</button>
+        :
+        <button onClick={()=>{
+          clearInterval(intervalTracker);
+          setIntervalTracker(-1);
+        }}>Stop</button>
+
+      }
+      
+      
+      <button onClick={()=>{
+        clearInterval(intervalTracker);
+        setIntervalTracker(-1);
+        setTime(0);
+      }}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
